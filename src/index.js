b4b3beb11463f6d044e9e5cd0108e4b9208b183e
Jürgen Kober <mindeleven@posteo.de>
2020-02-02 17:12:09 +0100

    Created the redux store and added a reducer.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 import { render } from "react-dom";
+import { createStore } from 'redux';
 import Counter from "./Counter";
 import "./index.css";
+
+function reducer(state, action) {
+  console.log('reducer', state, action);
+  return state;
+}
+
+const store = createStore(reducer);
 
 const App = () => (
   <div>
